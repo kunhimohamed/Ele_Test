e5fccee4b9cf7b4537ebea61745c01fb2310f050
kunhi_mohd <kunhi_mohd@bymacht.com>
2017-05-21 19:03:51 +0530

    bug fixed

--- server_side/analyseSms.py
+++ server_side/analyseSms.py
@@ -46,18 +46,21 @@ class ProcessFileSaveIntoDb(webapp2.RequestHandler):
 				continue
 			else:
 				MessageDict[each.split(':')[0].strip()[1:-1]] = "".join(each.split(':')[1:]).strip().replace(',','')
-		storedJson = SMSRepository()
-		storedJson.smsjson = json.dumps({'messages':jsonList})
-		Key = storedJson.put()
-		logging.info(Key)
-		time.sleep(5)
-		JsonDict = json.loads(Key.get().smsjson)
-		SenderNames = set([each.get("number",None) for each in JsonDict['messages']])
+		JsonDict = {'messages':jsonList}
+		SenderNames = [eachCont for eachCont in set([each.get("number",None) for each in JsonDict['messages']]) if eachCont]
+		SenderNamesFinalDict = {}
 		FrontEndDict = {}
-		logging.info(SenderNames)
-		# logging.info(JsonDict)
-		# FrontEndDict['total_number_of_senders'] = len(SenderNames)
-		for each in SenderNames:
+
+		for eachItem in SenderNames:
+			if (('+' in str(eachItem)[1:-1]) or (str(eachItem)[1:-1].isdigit())):
+				continue
+			elif ('-' in str(eachItem)[1:-1]):
+				if not (str(eachItem)[1:-1].split('-')[1].isdigit()):
+					SenderNamesFinalDict[eachItem] = str(eachItem)[1:-1].split('-')[1]
+			else:
+				SenderNamesFinalDict[eachItem] = str(eachItem)[1:-1]
+
+		for each in SenderNamesFinalDict:
 			TotalSmsCount = 0
 			TotalTextmsgCount = 0
 			TotalPromoCount = 0
@@ -69,10 +72,27 @@ class ProcessFileSaveIntoDb(webapp2.RequestHandler):
 						TotalPromoCount = TotalPromoCount + 1
 					else:
 						TotalTextmsgCount = TotalTextmsgCount + 1
-			FrontEndDict[str(each)[1:-1]] = [str(TotalSmsCount), str(TotalTextmsgCount), str(TotalPromoCount)]
+			FrontEndDict[SenderNamesFinalDict[each]] = [str(TotalSmsCount), str(TotalTextmsgCount), str(TotalPromoCount)]
+
+		wholeDict = {}
+		for each in SenderNamesFinalDict:
+			StoredList = []
+			for eachOne in JsonDict['messages']:
+				checkList = [eachData[1:-1] if (eachData.startswith('"') and eachData.endswith('"')) else eachData for eachData in eachOne.values()]
+				if str(each)[1:-1] in checkList:
+					StoredList.append(eachOne)
+			if ('-' in str(each)[1:-1]):
+				wholeDict[str(each)[1:-1].split('-')[1]] = StoredList
+			else:
+				wholeDict[str(each)[1:-1]] = StoredList
+
+		storedJson = SMSRepository()
+		storedJson.smsjson = json.dumps(wholeDict)
+		Key = storedJson.put()
+		logging.info(Key)
+		time.sleep(5)
 		memcache.set(key='fronEndDict', value=FrontEndDict)
 		memcache.set(key='jsonKey', value=Key)
-		logging.info(FrontEndDict)
 		self.response.headers['Content-Type'] = 'application/json'
 		self.response.out.write(json.dumps(FrontEndDict))
 
@@ -87,28 +107,16 @@ class DisplayToFront(webapp2.RequestHandler):
 class forCollapse(webapp2.RequestHandler):
 	"""docstring for forCollapse"""
 	def post(self):
-		logging.info(json.loads(self.request.body)["jsonkeyid"])
-		# jsonDict = ndb.Key('SMSRepository',int(json.loads(self.request.body)["jsonkeyid"])).get().smsjson
-		JsonDict = json.loads(ndb.Key('SMSRepository',int(json.loads(self.request.body)["jsonkeyid"])).get().smsjson)
-		wholeDict = {}
-		SenderNames = set([each.get("number",None) for each in JsonDict['messages']])
-		logging.info(SenderNames)
-		for each in SenderNames:
-			StoredList = []
-			for eachOne in JsonDict['messages']:
-				checkList = [eachData[1:-1] if (eachData.startswith('"') and eachData.endswith('"')) else eachData for eachData in eachOne.values()]
-				logging.info(checkList)
-				logging.info(each)
-				strlist = [str(eachContent) for eachContent in checkList]
-				logging.info(strlist)
-				if str(each)[1:-1] in strlist:
-					StoredList.append(eachOne)
-			wholeDict[str(each)[1:-1]] = StoredList
-		self.response.headers['Content-Type'] = 'application/json'
-		self.response.out.write(json.dumps(wholeDict))
-		# JsonDict = json.loads(Key('SMSRepository',json.loads(self.request.body)["jsonkeyid"]).get().smsjson)
-		# logging.info(JsonDict)
-
+		try:
+			logging.info(json.loads(self.request.body))
+			sender = json.loads(self.request.body)['sender']
+			JsonDict = json.loads(ndb.Key('SMSRepository',int(json.loads(self.request.body)["jsonkeyid"])).get().smsjson)
+			logging.info(JsonDict)
+			self.response.headers['Content-Type'] = 'application/json'
+			self.response.out.write(json.dumps({sender:JsonDict[sender]}))
+		except:
+			self.response.headers['Content-Type'] = 'application/json'
+			self.response.out.write(json.dumps({sender:[{'text':'could not find the key'}]}))	
 		
 
 application = webapp2.WSGIApplication([('/tobloburl', CreateBlobUrl),('/getbloburl', CreateBlobUrl),
